55e7fe197ad5c28fc94307e9915f5b21b88cab14
thiago.oliveira <thiago.oliveira@w6solutions.com.br>
2017-01-17 11:17:56 -0300

    add libreoffice service

--- content/libreoffice_iniciar.sh
+++ content/libreoffice_iniciar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/libreoffice_iniciar --nologo --norestore --invisible --headless --accept='socket,host=0,port=8100,tcpNoDelay=1;urp;'
+/usr/bin/libreoffice --nologo --norestore --invisible --headless --accept='socket,host=0,port=8100,tcpNoDelay=1;urp;'
